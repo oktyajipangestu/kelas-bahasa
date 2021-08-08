4dd12e9ac419ee49847d9a55b9b6e13b50bdd265
oktyajipangestu <oktyajipangestu@gmail.com>
2021-08-08 15:36:48 +0700

    perbaikan tampilan landing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,102 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 import Navbar from './components/Navbar';
 import {} from 'react-bootstrap';
 import Footer from './components/Footer';
+import { Link } from 'react-router-dom';
 
 function App() {
+  const [daftarKelas, setDaftarKelas] = useState([]);
+
+  useEffect(() => {
+    getKelas();
+  });
+
+  const getKelas = () => {
+    fetch(`http://127.0.0.1:8000/listKelas`, {
+      method: "GET",
+      body: JSON.stringify(),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((hasil) => {
+        setDaftarKelas(hasil.data);
+      });
+  }
+
+  console.log(daftarKelas);
   return (
     <div className="App">
       <header>
-        <Navbar />
-        <div class="jumbotron jumbotron-fluid">
-        <div class="container">
-          <h1 class="display-4">Asyik Berbahasa dengan SapaBahasa</h1>
-          <p class="lead">Pelajari berbagai bahasa dan tingkatkan kemampuan komunikasi kamu di SapaBahasa</p>
+          <Navbar />
+          <div class="jumbotron jumbotron-fluid">
+          <div class="container">
+            <div className="row d-flex flex-column">
+              <div className="col-9 align-items-center mt-5">
+                <h1 class="display-4"><b>Asyik Berbahasa dengan SapaBahasa</b></h1>
+                <p class="lead">Pelajari berbagai bahasa dan tingkatkan kemampuan komunikasi kamu di SapaBahasa</p>
+                <a href="#feature-heading" className="btn btn-info">Selengkapnya</a>
+                <Link className="btn btn-success my-2 my-sm-0 mx-2" to="/registrasiPelajar">Gabung</Link>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
       </header>
+      <main>
+          <div className="container">
+            <div id="feature-heading" className="row justify-content-center mb-5">
+              <div className="features-heading text-center my-5">
+                <h2><b>Kenapa Gabung SapaBahasa ?</b></h2>
+                <p>Yuk gabung, banyak manfaat dan kemudahan buat kamu dalam belajar bahasa di SapaBahasa</p>
+              </div>
+              <div className="col-lg-6 col-sm-12">
+                <img src="https://images.unsplash.com/photo-1577563908411-5077b6dc7624?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"  alt="percakapan" width="100%"/>
+              </div>
+              <div className="col-lg-6 col-sm-12">
+                <div className="d-flex flex-column justify-content-left">
+                  <div className="features-item mb-3">
+                    <h5><b>Belajar Dimana Saja dan Kapan Saja</b></h5>
+                    <p>Dengan kelas yang berbentuk pembelajar online, kamu akan lebih mudah untuk belajar dari mana saja dan kapan saja.</p>
+                  </div>
+                  <div className="features-item mb-3">
+                    <h5><b>Daftar Sekali Akses Sepuasnya</b></h5>
+                    <p>Dengan kelas yang berbentuk pembelajar online, kamu akan lebih mudah untuk belajar dari mana saja dan kapan saja.</p>
+                  </div>
+                  <div className="features-item mb-3">
+                    <h5><b>Diskusi Materi Sampai Bisa</b></h5>
+                    <p>Dengan fitur diskusi dalam setiap materi kamu bisa bertanya atau memberikan komentar pada yang lainnya.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+            
+          <div className="kelas-tersedia">
+            <div className="heading-kelas-tersedia text-center pt-5">
+              <h3><b>Kelas Bahasa Tersedia Untuk kamu</b></h3>
+              <p>Tersedia banyak pilihan kelas bahasa yang bisa kamu pelajari di SapaBahasa</p>
+            </div>
+            <div className="row justify-content-center mt-5">
+                {daftarKelas.map((data, index) => {
+                        return (
+                            <div key={index} className="col-md-6 col-lg-3 col-sm-12 item-class">
+                                <div class="card" style={{width: "18rem;"}}>
+                                    <img src={data.link_gambar} class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h5 class="card-title"><b>{data.judul}</b></h5>
+                                        <p class="card-text">{data.keterangan}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        );
+                    })}
+            </div>
+          </div>
+
+
+      </main>
       <Footer />
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,14 +11,14 @@ const Navbar = () => {
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav mr-auto">
+                        {/* <ul class="navbar-nav mr-auto">
                         <li class="nav-item active">
                             <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
                         </li>
-                        </ul>
-                        <form class="form-inline my-2 my-lg-0">
+                        </ul> */}
+                        <form class="form-inline my-2 my-lg-0 ml-auto">
                             <Link className="btn btn-outline-success my-2 my-sm-0 mx-2" to="/registrasiPelajar">Daftar</Link>
-                            <Link className="btn btn-outline-success my-2 my-sm-0 mx-2" to="/loginPelajar">login</Link>
+                            <Link className="btn btn-success my-2 my-sm-0 mx-2" to="/loginPelajar">login</Link>
                         </form>
                     </div>
                 </div>
